e854a750f0c410bccb7f03ea8ed3d32a8ac5a630
csantosm <4a.santos@gmail.com>
2020-10-01 16:58:50 +0200

    Test ionic

--- openvidu-ionic/src/app/app.component.ts
+++ openvidu-ionic/src/app/app.component.ts
@@ -35,6 +35,19 @@ export class AppComponent implements OnDestroy {
     mySessionId: string;
     myUserName: string;
 
+    global = {
+        pcSend: null,
+        pcRecv: null,
+        statsInterval: null,
+
+        // Memory used to calculate averages and rates.
+        printWebRtcStats: { bytesSent: 0 },
+    };
+
+    ui: any = {};
+
+
+
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
@@ -54,8 +67,277 @@ export class AppComponent implements OnDestroy {
             if (this.platform.is('ios') && this.platform.is('cordova')) {
                 cordova.plugins.iosrtc.registerGlobals();
             }
+
+
+            this.ui = {
+                // Inputs
+                start: false,
+                stop: false,
+
+              };
+
+
+            this.ui.stop = true;
+
+            const logMethod = console.log;
+            const logMessages = [];
+
+            console.log = function () {
+                logMessages.push.apply(logMessages, arguments);
+
+                const console = document.getElementById("uiConsole");
+                console.innerHTML = logMessages.reduce(
+                (acc, cur) => acc + cur + "<br>",
+                ""
+                );
+                logMethod.apply(console, arguments);
+            };
+
+
+
         });
     }
+
+    async startWebrtc() {
+
+        await this.checkAndroidPermissions();
+        // RTCPeerConnection setup.
+        this.startWebrtcPc();
+
+        // SDP Offer/Answer negotiation.
+        this.startWebrtcSdp();
+
+        // Media flow.
+        await this.startWebrtcMedia();
+
+        // Statistics.
+        await this.startWebrtcStats();
+
+        // Update UI.
+        this.ui.start = true;
+        this.ui.stop = false;
+      }
+
+      startWebrtcPc() {
+        const pcSend = new RTCPeerConnection();
+        this.global.pcSend = pcSend;
+
+        const pcRecv = new RTCPeerConnection();
+        this.global.pcRecv = pcRecv;
+
+        async function onIceCandidate(iceEvent, pc) {
+          if (iceEvent.candidate) {
+            // Send the candidate to the remote peer.
+            try {
+              await pc.addIceCandidate(iceEvent.candidate);
+            } catch (error) {
+              console.error("[onIceCandidate] Error:", error);
+            }
+          } else {
+            console.log("[onIceCandidate] All ICE candidates have been sent");
+          }
+        }
+
+        pcSend.addEventListener("icecandidate", (iceEvent) =>
+          onIceCandidate(iceEvent, pcRecv)
+        );
+        pcRecv.addEventListener("icecandidate", (iceEvent) =>
+          onIceCandidate(iceEvent, pcSend)
+        );
+      }
+
+      startWebrtcSdp() {
+        const pcSend = this.global.pcSend;
+        const pcRecv = this.global.pcRecv;
+
+        pcSend.addEventListener("negotiationneeded", async () => {
+          console.log("[on pcSend.negotiationneeded]");
+
+          try {
+            const sdpOffer = await pcSend.createOffer();
+            await pcSend.setLocalDescription(sdpOffer);
+            await pcRecv.setRemoteDescription(pcSend.localDescription);
+            // console.log("[pcSend] SDP Offer:", pcSend.localDescription.sdp);
+
+            const sdpAnswer = await pcRecv.createAnswer();
+            await pcRecv.setLocalDescription(sdpAnswer);
+            await pcSend.setRemoteDescription(pcRecv.localDescription);
+            // console.log("[pcRecv] SDP Answer:", pcRecv.localDescription.sdp);
+          } catch (err) {
+            console.error("[on pcSend.negotiationneeded] Error:", err);
+          }
+        });
+
+        pcRecv.addEventListener("iceconnectionstatechange", () => {
+          console.log(
+            "[on pcRecv.iceconnectionstatechange] pcRecv.iceConnectionState:",
+            pcRecv.iceConnectionState
+          );
+        });
+      }
+
+      async startWebrtcMedia() {
+        const pcSend = this.global.pcSend;
+        const pcRecv = this.global.pcRecv;
+
+        pcRecv.addEventListener("track", (trackEvent) => {
+          console.log(
+            `[on pcRecv.track] kind: ${trackEvent.track.kind}, direction: ${trackEvent.transceiver.direction}`
+          );
+
+          // Show the stream and start playback.
+          // NOTE: Playback doesn't start automatically because the <video> element
+          // is not "autoplay", which is an attribute that we recommend avoiding.
+          this.ui.remoteVideo.srcObject = trackEvent.streams[0];
+          this.ui.remoteVideo.play();
+        });
+
+        let localStream;
+        try {
+          localStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+          });
+        } catch (err) {
+          console.error("[startWebrtcMedia] Error:", err);
+          return;
+        }
+
+        // Show the stream and start playback.
+        // NOTE: Playback doesn't start automatically because the <video> element
+        // is not "autoplay", which is an attribute that we recommend avoiding.
+        this.ui.localVideo.srcObject = localStream;
+        this.ui.localVideo.play();
+
+        // Add the new tracks to the sender PeerConnection.
+        for (const track of localStream.getTracks()) {
+          // NOTE: addTrack() causes creation of a "sendrecv" RTCRtpTransceiver.
+          //       https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addTrack#New_senders
+          // NOTE: addTrack() triggers event "negotiationneeded".
+          const sender = pcSend.addTrack(track, localStream);
+
+          // Log the new track and its corresponding transceiver's direction.
+          const tc = pcSend.getTransceivers().find((tc) => tc.sender === sender);
+          console.log(
+            `[pcSend.addTrack] kind: ${track.kind}, direction: ${tc.direction}`
+          );
+        }
+      }
+
+      async startWebrtcStats() {
+        // Retrieve stats once per second; this is needed to calculate values such as
+        // bitrates (bits per second) or interval losses (packets lost per second).
+        const intervalID = setInterval(async () => {
+          const pc = this.global.pcSend;
+
+          // RTCStatsReport behaves like a Map. Each value is an RTCStats-derived
+          // object, where "type" is one of the RTCStatsType enum.
+          //
+          // Doc:
+          // - RTCStatsReport: https://w3c.github.io/webrtc-pc/#dom-rtcstatsreport
+          // - RTCStats: https://w3c.github.io/webrtc-pc/#dom-rtcstats
+          // - RTCStatsType: https://w3c.github.io/webrtc-stats/#dom-rtcstatstype
+          const statsMap = await pc.getStats();
+
+          // DEBUG - Print all contents of the RTCStatsReport.
+          // statsMap.forEach((stats) => console.log(JSON.stringify(stats)));
+
+          // printWebRtcStats(statsMap);
+
+          // Print all stats that contain "frameWidth"
+          statsMap.forEach((stats) => {
+            if ("frameWidth" in stats) {
+              console.log(
+                `stats type: ${stats.type}, frameWidth: ${stats.frameWidth}`
+              );
+            }
+          });
+        }, 1000);
+        this.global.statsInterval = intervalID;
+      }
+
+
+      printWebRtcStats(statsMap) {
+        // Filter and match stats, to find the wanted values
+        // (report only from first video track that is found)
+
+        // Note: in TypeScript, most of these would be using the '?' operator.
+
+        const localOutVideoStats:any = Array.from(statsMap.values()).find(
+          (stats: any) => stats.type === "outbound-rtp" && stats.kind === "video"
+        );
+        const remoteInVideoStats = statsMap.get(localOutVideoStats.remoteId);
+        const codecStats = statsMap.get(localOutVideoStats.codecId);
+        const transportStats = statsMap.get(localOutVideoStats.transportId);
+        const candidatePairStats = statsMap.get(
+          transportStats.selectedCandidatePairId
+        );
+        const localCandidateStats = statsMap.get(candidatePairStats.localCandidateId);
+        const remoteCandidateStats = statsMap.get(
+          candidatePairStats.remoteCandidateId
+        );
+
+        // Calculate per-second values.
+        const bytesSentPerS =
+          localOutVideoStats.bytesSent - this.global.printWebRtcStats.bytesSent;
+
+        // Update values in memory, for the next iteration.
+        this.global.printWebRtcStats.bytesSent = localOutVideoStats.bytesSent;
+
+        // Prepare data and print all values.
+        const bitrateSentKbps = (bytesSentPerS * 8) / 1000.0;
+        const availableInBitrateKbps = candidatePairStats.availableIncomingBitrate
+          ? candidatePairStats.availableIncomingBitrate / 1000.0
+          : 0;
+        const availableOutBitrateKbps = candidatePairStats.availableOutgoingBitrate
+          ? candidatePairStats.availableOutgoingBitrate / 1000.0
+          : 0;
+        let data: any = {};
+        data.localSsrc = localOutVideoStats.ssrc;
+        data.remoteSsrc = remoteInVideoStats.ssrc;
+        data.codec = codecStats.mimeType;
+        data.localPort = localCandidateStats.port;
+        data.remotePort = remoteCandidateStats.port;
+        data.packetsSent = localOutVideoStats.packetsSent;
+        data.retransmittedPacketsSent = localOutVideoStats.retransmittedPacketsSent;
+        data.bytesSent = localOutVideoStats.bytesSent;
+        data.nackCount = localOutVideoStats.nackCount;
+        data.firCount = localOutVideoStats.firCount ? localOutVideoStats.firCount : 0;
+        data.pliCount = localOutVideoStats.pliCount ? localOutVideoStats.pliCount : 0;
+        data.sliCount = localOutVideoStats.sliCount ? localOutVideoStats.sliCount : 0;
+        data.iceRoundTripTime = candidatePairStats.currentRoundTripTime;
+        data.bitrateSentKbps = bitrateSentKbps;
+        data.availableInBitrateKbps = availableInBitrateKbps;
+        data.availableOutBitrateKbps = availableOutBitrateKbps;
+
+        console.log("[printWebRtcStats] VIDEO STATS:", data);
+      }
+
+      stopWebrtc() {
+        clearInterval(this.global.statsInterval);
+
+        const localVideo: any = document.getElementById("uiLocalVideo");
+        const remoteVideo: any = document.getElementById("uiRemoteVideo");
+
+        localVideo.pause();
+        localVideo.srcObject = null;
+        remoteVideo.pause();
+        remoteVideo.srcObject = null;
+
+        this.global.pcSend.close();
+        this.global.pcSend = null;
+        this.global.pcRecv.close();
+        this.global.pcRecv = null;
+
+        // Update UI.
+        this.ui.start = false;
+        this.ui.stop = true;
+
+        console.log("[stopWebrtc] Stopped");
+      }
+
+
+
 
     @HostListener('window:beforeunload')
     beforeunloadHandler() {
@@ -67,6 +349,8 @@ export class AppComponent implements OnDestroy {
         // On component destroyed leave session
         this.leaveSession();
     }
+
+
 
     joinSession() {
         // --- 1) Get an OpenVidu object ---
